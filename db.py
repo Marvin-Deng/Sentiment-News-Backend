28be339dd2645c64bf1e51325a4abba3a3ce4c69
Marvin <mdengomg2@gmail.com>
2023-12-18 23:05:36 -0800

    Moved backend code to new repo

---
+++ db.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+from tortoise import Tortoise
+
+load_dotenv()
+
+DB_CONFIG = {
+    "connections": {
+        "default": os.getenv('DATABASE_URL'),
+    },
+    "apps": {
+        "models": {
+            "models": ["models.ArticleModel", "models.TickerModel"],
+            "default_connection": "default",
+        }
+    }
+}
+
+async def init_db():
+    await Tortoise.init(config=DB_CONFIG)
+    await Tortoise.generate_schemas()
