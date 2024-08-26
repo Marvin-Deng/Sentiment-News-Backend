82d3f35964e94ebeb17dcee882a66f8d40183834
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-08-26 00:45:53 -0700

    Remove unused fields from ticker schema

--- src/models/ticker.py
+++ src/models/ticker.py
@@ -11,17 +11,7 @@ class TickerModel(Model):
     market_date = CharField(max_length=100)
     ticker = CharField(max_length=25, null=True)
     open_price = FloatField(null=True)
-    high_price = FloatField(null=True)
-    low_price = FloatField(null=True)
     close_price = FloatField(null=True)
-    volume = IntField(null=True)
-    adj_open = FloatField(null=True)
-    adj_high = FloatField(null=True)
-    adj_low = FloatField(null=True)
-    adj_close = FloatField(null=True)
-    adj_volume = IntField(null=True)
-    div_cash = FloatField(null=True)
-    split_factor = FloatField(null=True)
 
     class Meta:
         table = "ticker_model"
--- src/utils/stock_utils.py
+++ src/utils/stock_utils.py
@@ -14,44 +14,18 @@ def get_stock_info(ticker: str, date_str: str) -> dict:
     _, eod_data, _ = stock_services.get_eod_data_tinngo(
         ticker=ticker, start_date=date_str, end_date=date_str
     )
-
-    stock_info = {
-        "open_price": None,
-        "high_price": None,
-        "low_price": None,
-        "close_price": None,
-        "volume": None,
-        "adj_open": None,
-        "adj_high": None,
-        "adj_low": None,
-        "adj_close": None,
-        "adj_volume": None,
-        "div_cash": None,
-        "split_factor": None,
-    }
-
+    stock_info = {}
     if (
         eod_data
         and isinstance(eod_data, list)
         and len(eod_data) > 0
         and isinstance(eod_data[0], dict)
     ):
-        keys_mapping = {
+        key_mapping = {
             "open_price": "open",
-            "high_price": "high",
-            "low_price": "low",
             "close_price": "close",
-            "volume": "volume",
-            "adj_open": "adjOpen",
-            "adj_high": "adjHigh",
-            "adj_low": "adjLow",
-            "adj_close": "adjClose",
-            "adj_volume": "adjVolume",
-            "div_cash": "divCash",
-            "split_factor": "splitFactor",
         }
-
-        for key, eod_key in keys_mapping.items():
+        for key, eod_key in key_mapping.items():
             stock_info[key] = eod_data[0].get(eod_key, None)
 
     return stock_info
